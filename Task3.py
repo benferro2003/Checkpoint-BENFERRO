213cd82eeb37430b5db9a177243efd9b34a86f67
benferro2003 <74298155+benferro2003@users.noreply.github.com>
2022-10-25 15:06:35 +0100

    Update Task3.py

--- Task3.py
+++ Task3.py
@@ -1,1 +1,9 @@
-
+#!/usr/bin/env python3
+if __name__ == '__main__':
+    print('Checkpoint task 3 - BEN FERRO')
+    password = 'banana'
+    guess = input('Greetings! What is the password?')
+    while guess != password:
+        print('incorrect! you may try again.')
+        guess = input('Greetings! What is the password?')
+    print("Correct! You may enter.")
