d8e6584a64c9b592048c1e8ade097d5f206e0e1c
benferro2003 <74298155+benferro2003@users.noreply.github.com>
2022-10-25 14:52:05 +0100

    Update Task2.py

--- Task2.py
+++ Task2.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
-def cafe():
-    slices = int(input('How many slices of spam one requires?'))
-    spam = 'spam,'
-    print(f'Egg with {spam * slices} coming up!')
-
-
 if __name__ == '__main__':
     print('Checkpoint task 2 - BEN FERRO')
-    cafe()
+    slices = int(input('How many slices of spam one requires?'))
+    while slices < 1:
+        slices = int(input('How many slices of spam one requires?'))
+
+    spam = 'spam, '
+    if slices == 2:
+        print(f'Egg with {spam}, and spam coming up!')
+    elif slices == 1:
+        print(print(f'Egg with {spam} coming up!'))
+
+    else:
+        print(f'Egg with {spam * (slices-1)}and spam coming up!')
