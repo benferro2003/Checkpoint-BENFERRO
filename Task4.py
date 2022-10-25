3a6cd59e8a3a2e58cd95a2c8e565517d3a830f88
benferro2003 <74298155+benferro2003@users.noreply.github.com>
2022-10-25 15:15:21 +0100

    Update Task4.py

--- Task4.py
+++ Task4.py
@@ -1,1 +1,24 @@
+#!/usr/bin/env python3
+def password_checker(password):
+    from string import ascii_letters as letters, digits, punctuation
 
+    has_letter = has_digit = has_punc = False
+
+    for character in password:
+        if character in letters:
+            has_letter = True
+        elif character in digits:
+            has_digit = True
+        elif character in punctuation:
+            has_punc = True
+
+    if has_letter == True and has_digit == True and has_punc == True:
+        print("The password is acceptable")
+    else:
+        print('The password is unacceptable')
+
+
+if __name__ == '__main__':
+    print('Checkpoint task 4 - BEN FERRO')
+    password_checker('Ben0!')
+    password_checker('password')
