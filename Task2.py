da4928b8f5e5c6a41485b37e8d3f4a790519e20b
benferro2003 <74298155+benferro2003@users.noreply.github.com>
2022-10-25 14:20:56 +0100

    Update Task2.py

--- Task2.py
+++ Task2.py
@@ -1,1 +1,10 @@
+#!/usr/bin/env python3
+def cafe():
+    slices = int(input('How many slices of spam one requires?'))
+    spam = 'spam,'
+    print(f'Egg with {spam * slices} coming up!')
 
+
+if __name__ == '__main__':
+    print('Checkpoint task 2 - BEN FERRO')
+    cafe()
