0a724a489fc4582163dff0ef94925826428ea187
benferro2003 <74298155+benferro2003@users.noreply.github.com>
2022-10-25 14:08:38 +0100

    Update Task1.py

--- Task1.py
+++ Task1.py
@@ -1,1 +1,12 @@
+#!/usr/bin/env python3
+def title():
+    name = input('Enter thy Name!')
+    if name.startswith('Lord') or name.startswith('Lady'):
+        print(f'It shall be so, {name}!')
+    else:
+        print(f'You may not be known by that name!')
 
+
+if __name__ == '__main__':
+    print('Checkpoint task 1 - BEN FERRO')
+    title()
